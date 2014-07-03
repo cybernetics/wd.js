d16eaa1fc90d9c6e3385104f40e97d26432b9c2b
John Wu <webmaster@leapoahead.com>
2014-07-03 08:09:12 +0800

    yuidoc support

--- src/es5-shim/array.prototype.map.js
+++ src/es5-shim/array.prototype.map.js
@@ -1,7 +1,21 @@
+/**
+ * 自带的ES5 Shim里集成了一些ES5当中最常用的函数，用于加快编码效率。注意，这个类并不在全局对象
+ * `wd`里存在。
+ * 
+ * @class ES5Shim
+ */
 ;(function() {
   if (!Array.prototype.map) {
+
     /**
+     * ES5-Array.prototype.map
      * 
+     * @method  map
+     * @for ES5Shim
+     * @example
+     *     var squareOfNumbers = [1, 2, 3].map(function(num) {
+     *       return Math.pow(num, 2);
+     *     });
      */
     Array.prototype.map = function(callback, thisArg) {
       var len = this.length,
